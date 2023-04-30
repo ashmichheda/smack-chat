5f2cde4084f6a5af4918635b55bf5c2790d72c9c
ashmi.chheda <ashmi.chheda@servicenow.com>
2023-04-30 10:42:08 -0700

    refactor: polish create user activity

--- app/src/main/java/com/example/smack/Controller/CreateUserActivity.kt
+++ app/src/main/java/com/example/smack/Controller/CreateUserActivity.kt
@@ -1,24 +1,30 @@
 package com.example.smack.Controller
 
+import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.ImageView
-import android.widget.TextView
+import android.widget.*
 import androidx.core.widget.AutoScrollHelper
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.example.smack.R
 import com.example.smack.Services.AuthService
+import com.example.smack.Utilities.BROADCAST_USER_DATA_CHANGE
 import java.util.*
 
 class CreateUserActivity : AppCompatActivity() {
 
     var userAvatar = "profileDefault"
     var avatarColor = "[0.5, 0.5, 0.5, 1]"
+    private lateinit var createSpinner: ProgressBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_user)
+        createSpinner = findViewById<ProgressBar>(R.id.createSpinner)
+        createSpinner.visibility = View.INVISIBLE
+
     }
 
     fun generateUserAvatar(view: View) {
@@ -53,25 +59,59 @@ class CreateUserActivity : AppCompatActivity() {
     }
 
     fun createUserClicked(view: View) {
+        enableSpinner(true)
         val userName = findViewById<TextView>(R.id.createUserNameTxt).text.toString()
         val email: String = findViewById<TextView?>(R.id.createEmailTxt).text.toString()
         val password = findViewById<TextView>(R.id.createPasswordTxt).text.toString()
 
-        AuthService.registerService(this, email, password) {registerSuccess ->
-            if (registerSuccess) {
-                println("User successfully registered")
-                AuthService.loginUser(this, email, password) {loginSuccess ->
-                    if (loginSuccess) {
-                        AuthService.createUser(this, userName, email, userAvatar, avatarColor) {createSuccess ->
-                            if (createSuccess) {
-                                finish()
+        // input checks to make sure is not empty
+        if (userName.isNotEmpty() && email.isNotEmpty() && password.isNotEmpty()) {
+            AuthService.registerService(this, email, password) {registerSuccess ->
+                if (registerSuccess) {
+                    println("User successfully registered")
+                    AuthService.loginUser(this, email, password) {loginSuccess ->
+                        if (loginSuccess) {
+                            AuthService.createUser(this, userName, email, userAvatar, avatarColor) {createSuccess ->
+                                if (createSuccess) {
+                                    val userDataChange = Intent(BROADCAST_USER_DATA_CHANGE)
+                                    LocalBroadcastManager.getInstance(this).sendBroadcast(userDataChange)
+                                    enableSpinner(false)
+                                    finish()
+                                } else {
+                                    errorToast()
+                                }
                             }
+                        } else {
+                            errorToast()
                         }
                     }
+                } else {
+                    errorToast()
                 }
-            } else {
-                println("Error in user registration!.")
             }
+        } else {
+            Toast.makeText(this, "Make sure username, email & password are filled in.", Toast.LENGTH_LONG).show()
+            enableSpinner(false)
         }
+    }
+
+    fun errorToast() {
+        Toast.makeText(this, "Something went wrong, Please try again.", Toast.LENGTH_LONG).show()
+        enableSpinner(false)
+    }
+
+    fun enableSpinner(enable: Boolean) {
+        val createUserBtn = findViewById<Button>(R.id.createUserBtn)
+        val createAvatarImageView = findViewById<ImageView>(R.id.createAvatarImgView)
+        val backgroundColorBtn = findViewById<Button>(R.id.backgroundColorBtn)
+
+        if (enable) {
+            createSpinner.visibility = View.VISIBLE
+        } else {
+            createSpinner.visibility = View.INVISIBLE
+        }
+        createUserBtn.isEnabled = !enable
+        createAvatarImageView.isEnabled = !enable
+        backgroundColorBtn.isEnabled = !enable
     }
 }
