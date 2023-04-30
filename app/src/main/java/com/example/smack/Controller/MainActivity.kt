7152de73adc7589a8177b82dbb807f3813c242a4
ashmi.chheda <ashmi.chheda@servicenow.com>
2023-04-30 14:04:11 -0700

    feat: add broadcast receiver and finish up UI

--- app/src/main/java/com/example/smack/Controller/MainActivity.kt
+++ app/src/main/java/com/example/smack/Controller/MainActivity.kt
@@ -1,8 +1,15 @@
 package com.example.smack.Controller
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
+import android.graphics.Color
 import android.os.Bundle
+import android.service.autofill.UserData
 import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -11,7 +18,11 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.example.smack.R
+import com.example.smack.Services.AuthService
+import com.example.smack.Services.UserDataService
+import com.example.smack.Utilities.BROADCAST_USER_DATA_CHANGE
 import com.example.smack.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -38,6 +49,30 @@ class MainActivity : AppCompatActivity() {
 
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        // Register a broadcast receiver
+        LocalBroadcastManager.getInstance(this).registerReceiver(userDataChangeReceiver, IntentFilter(
+            BROADCAST_USER_DATA_CHANGE))
+    }
+
+    private val userDataChangeReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            val userNameNavHeader = findViewById<TextView>(R.id.userNameNavHeader)
+            val userEmailNavHeader = findViewById<TextView>(R.id.userEmailNavHeader)
+            val loginBtnNavHeader = findViewById<TextView>(R.id.loginBtnNavHeader)
+            val userImageNavHeader = findViewById<ImageView>(R.id.userImageNavHeader)
+
+            // update the UI with newly created user data
+            if (AuthService.isLoggedIn) {
+                // update the UI elements of the nav header
+                userNameNavHeader.text = UserDataService.name
+                userEmailNavHeader.text = UserDataService.email
+                val resourceId = resources.getIdentifier(UserDataService.avatarName, "drawable", packageName)
+                userImageNavHeader.setBackgroundColor(UserDataService.returnAvatarColor(UserDataService.avatarColor))
+                userImageNavHeader.setImageResource(resourceId)
+                loginBtnNavHeader.text = "Logout"
+            }
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
@@ -46,9 +81,26 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun loginBtnNavClicked(view: View) {
-        // Transition to LoginActivity page
-        val loginIntent = Intent(this, LoginActivity::class.java)
-        startActivity(loginIntent)
+
+        if (AuthService.isLoggedIn) {
+            // log out
+            UserDataService.logout()
+            val userNameNavHeader = findViewById<TextView>(R.id.userNameNavHeader)
+            val userEmailNavHeader = findViewById<TextView>(R.id.userEmailNavHeader)
+            val userImageNavHeader = findViewById<ImageView>(R.id.userImageNavHeader)
+            val loginBtnNavHeader = findViewById<TextView>(R.id.loginBtnNavHeader)
+
+            userNameNavHeader.text = "Login"
+            userEmailNavHeader.text = ""
+            userImageNavHeader.setImageResource(R.drawable.profiledefault)
+            userImageNavHeader.setBackgroundColor(Color.TRANSPARENT)
+            loginBtnNavHeader.text = "Login"
+
+        } else {
+            // Transition to LoginActivity page
+            val loginIntent = Intent(this, LoginActivity::class.java)
+            startActivity(loginIntent)
+        }
     }
 
     fun addChannelClicked(view: View) {
